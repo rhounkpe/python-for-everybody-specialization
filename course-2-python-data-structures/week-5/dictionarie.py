ade10efc975d0f5f3cac072f785ccf8ae0267afc
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-21 10:09:09 +0100

    Course 5: Dictionaries

---
+++ course-2-python-data-structures/week-5/dictionarie.py
@@ -0,0 +1,15 @@
+# Dictionaries
+"""
+- Lists index their entries based on the position in the list
+- Dictionaries are like bags - no order
+- SO we index the things we put in the dictionary with a 'lookup tag'
+"""
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse)
+print(purse['candy'])
+
+purse['candy'] = purse['candy'] + 2
+print(purse)

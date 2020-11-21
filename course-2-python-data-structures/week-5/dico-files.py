0faccf7dc716c9208cdff71ee8ab35f382e4734b
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-21 12:06:56 +0100

    Retrieving lists of keys and values

--- course-2-python-data-structures/week-5/dico-files.py
+++ course-2-python-data-structures/week-5/dico-files.py
@@ -29,3 +29,14 @@ actually it goes through all of the keys in the dictionary and looks up the valu
 new_counts = { 'chuck': 1, 'fred': 42, 'jan': 100 }
 for key in new_counts:
     print(key, new_counts[key])
+
+
+# Retrieving lists of keys and values
+"""
+You can get a list of keys, values, or items (both) from a dictionary.
+"""
+jjj = { 'chuck': 1, 'fred': 42, 'jan': 100 }
+print(list(jjj))
+print(jjj.keys())
+print(jjj.values())
+print(jjj.items())
